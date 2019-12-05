5843fc76c4577813c4fa92b3ab621c19e7b69b63
1977870073 <1977870073@qq.com>
2019-12-05 09:11:19 +0800

    修改后端注解

--- renting/zfxxgl_admin_hd/renting_Admin/src/main/java/com/cozyBed/renting_Admin/service/HouseInfoService.java
+++ renting/zfxxgl_admin_hd/renting_Admin/src/main/java/com/cozyBed/renting_Admin/service/HouseInfoService.java
@@ -15,24 +15,70 @@ import java.util.List;
  * @date 2019/4/1
  */
 public interface HouseInfoService {
-    //返回查询的房源信息
+    /**
+     * 返回查询的房源信息
+     */
     public List<RentHouseinfoWithBLOBs> selectInfoes(Choose choose)throws Exception;
-    //返回查询的房源信息数量
+
+    /**返回查询的房源信息数量
+     *
+     * @param choose
+     * @return
+     * @throws Exception
+     */
     public long infoCount(Choose choose)throws Exception;
-    //返回RentHouseinfoWithBLOBsExpand指定的房源信息
+    /**
+     * 返回RentHouseinfoWithBLOBsExpand指定的房源信息
+     */
     public RentHouseinfoWithBLOBsExpand findByPrimaryKey(Integer id)throws Exception;
-    //返回RentHouseinfoWithBLOBs指定的房源信息
+    /**
+     * 返回RentHouseinfoWithBLOBs指定的房源信息
+     */
     public RentHouseinfoWithBLOBs findByPrimaryKeyOther(Integer id)throws Exception;
-    //返回搜索数据的前40条
+    /**
+     * 返回搜索数据的前40条
+     */
     public List<RentHouseinfoWithBLOBs> selectBySearch(String search)throws Exception;
-    //插入房源信息
+
+    /**
+     * 插入房源信息
+     * @param info
+     * @return
+     * @throws Exception
+     */
     public int insertInfo(RentHouseinfoWithBLOBs info)throws Exception;
-    //出租房源信息
+
+    /**
+     * 出租房源信息
+     * @param user
+     * @param index
+     * @return
+     * @throws Exception
+     */
     public List<RentHouseinfoWithBLOBs> selectByUser(String user, Integer index)throws Exception;
-    //查询出租房源数量
+
+    /**
+     * 查询出租房源数量
+     * @param user
+     * @return
+     * @throws Exception
+     */
     public long selectByUserCount(String user)throws Exception;
-    //修改房源信息
+
+    /**
+     * 修改房源信息
+     * @param record
+     * @return
+     * @throws Exception
+     */
     public int updateByPrimaryKeySelective(RentHouseinfoWithBLOBs record)throws Exception;
-    //删除房源信息
+
+    /**
+     * 删除房源信息
+     * @param id
+     * @return
+     * @throws Exception
+     */
     public int deleteByPrimaryKey(Integer id)throws Exception;
+
 }
