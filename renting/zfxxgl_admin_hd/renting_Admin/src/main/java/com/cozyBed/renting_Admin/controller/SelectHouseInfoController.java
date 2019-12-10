1b2d83f8d1b9e6e267f0ef34685beb0c11fbd26e
1977870073 <1977870073@qq.com>
2019-12-10 09:23:19 +0800

    修改审批房源页面

--- renting/zfxxgl_admin_hd/renting_Admin/src/main/java/com/cozyBed/renting_Admin/controller/SelectHouseInfoController.java
+++ renting/zfxxgl_admin_hd/renting_Admin/src/main/java/com/cozyBed/renting_Admin/controller/SelectHouseInfoController.java
@@ -5,6 +5,7 @@ import com.cozyBed.renting_Admin.po.Choose;
 import com.cozyBed.renting_Admin.po.Page;
 import com.cozyBed.renting_Admin.po.RentHouseinfoWithBLOBs;
 import com.cozyBed.renting_Admin.service.HouseInfoService;
+import com.cozyBed.renting_Admin.utils.ObjectUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +31,7 @@ public class SelectHouseInfoController {
         String index = request.getParameter("index");
         int i = 0;
         Choose c = new Choose();
-        if(index==""||index==null){
+        if(ObjectUtil.isEmply(index)){
             i=1;
         }else{
             i=Integer.parseInt(index);
@@ -39,6 +40,6 @@ public class SelectHouseInfoController {
         Page page =  page = new Page(i,10,(int)houseInfoService.infoCount(c));
         List<RentHouseinfoWithBLOBs> list = houseInfoService.selectInfoes(c);
         Object[] obj = new Object[]{list, page};
-        return JSON.toJSONString(obj);
+        return JSON.toJSONString(list);
     }
 }
