9ee0004839601fc37e474d8df354c672ce60a4f7
1977870073 <1977870073@qq.com>
2019-12-11 14:50:31 +0800

    修改审批页面

--- renting/zfxxgl_admin_qd/web/static/js/old/approval.js
+++ renting/zfxxgl_admin_qd/web/static/js/old/approval.js
@@ -1,7 +1,44 @@
 window.onload = function(){
     BIGblAndbrFun();
     SMALLblAndbrFun();
+	//获取页面数据
+	var cp01 = getQueryVariable("cp01");
+	if(!isEmpty(cp01)){
+		$.ajax({
+			type: 'get',
+			url: '/web/do/approval/selectInfo',
+			data: {"bh", cp01},
+			dataType: 'text',
+			success: function(data){
+				init(data);
+			},
+			error: function(errorMsg){
+				layui.msg()
+			}
+		});
+	}
 }
+
+//获取链接中的参数
+function getQueryVariable(variable)
+{
+   var query = window.location.search.substring(1);
+   var vars = query.split("&");
+   for (var i=0;i<vars.length;i++) {
+	   var pair = vars[i].split("=");
+	   if(pair[0] == variable){return decodeURI(pair[1]);}//改动
+   }
+   return false;
+}
+//判断变量是否为空
+function isEmpty(obj){
+    if(typeof obj == "undefined" || obj == null || obj == ""){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 var afterImg = 0;
 // 小图片左右事件
 function SMALLblAndbrFun(){
@@ -193,4 +230,73 @@ function  fadeOut(obj,time) {
         }
         obj.style.opacity=1-prop;//透明度公式： 初始值+系数*（结束值-初始值）
     },13);//每隔13ms执行一次function函数
+}
+
+function init(data){
+	$('#title').text(data['title']);
+	$('#rentPrice').text(data['price']);
+	$('#rental_mode').text(data['rentalMode']);
+	$('#house_type').text(data['houseType']);
+	$('#rentArea').text(data['area']);
+	$('#orientation').text(data['orientation']);
+	$('#rentFloor').text(data['floor']);
+	$('#rentAllFloor').text(data['allfloor']);
+	$('#renovation').text(data['renovation']);
+	$('#residential_areas').text(data['residentialAreas']+"("+data['district']+")";
+	$('#residential_areas2').text(data['residentialNote']);
+	$('#rentAddress').text(data['address']);
+	$('#cName').text(data['userName']);
+	$('#cPhone').text(data['userPhone']);
+	var pic = data['pic'];
+	var bigHtml = "";
+	var litHtml = "";
+	for(var i=0; i<pic.length; i++){
+		bigHtml +="<img width='690px' height='440px' src='"+pic[i]+"'>";
+		litHtml +="<li><img src='"+pic[i]+"'></li>";
+	}
+	$('#bigImg').append(bigHtml);
+	$('#litImg').append(litHtml);
+	$('#tYes').click(function(){
+		tYes(data['id'],data['user']);
+	});
+	$('#tNo').click(function(){
+		tNo(data['id'],data['user']);
+	});
+	
+}
+
+function tYes(id,user) {
+	$.ajax({
+		url: '/web/do/approval/tYes',
+		type: 'post',
+		data: {"id":id,"user":user},
+		dataType: 'text',
+		success: function (data) {
+			console.log(data)
+			if(data=="success"){
+				alert("操作成功！");
+				window.location.href = '/web/zf/html/htgl/shenpi.html';
+			}else{
+				alert("操作失败！");
+			}
+		}
+	})
+}
+
+function tNo(id,user) {
+	$.ajax({
+		url: '/web/do/approval/tNo',
+		type: 'post',
+		data: {"id":id,"uu":user},
+		dataType: 'text',
+		success: function (data) {
+			console.log(data)
+			if(data=="success"){
+				alert("操作成功！");
+				window.location.href = '/web/zf/html/htgl/shenpi.html';
+			}else{
+				alert("操作失败！");
+			}
+		}
+	})
 }
