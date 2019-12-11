8df22d61ea37e296961800c8158578e0974315f7
1977870073 <1977870073@qq.com>
2019-12-11 10:09:04 +0800

    修改审批页面

---
+++ renting/zfxxgl_admin_qd/web/static/js/old/approval.js
@@ -0,0 +1,196 @@
+window.onload = function(){
+    BIGblAndbrFun();
+    SMALLblAndbrFun();
+}
+var afterImg = 0;
+// 小图片左右事件
+function SMALLblAndbrFun(){
+    var litImg =document.getElementsByClassName("litImg")[0];
+    var list = litImg.getElementsByTagName("li");
+    var mbl = document.getElementsByClassName("mbl")[0];
+    var mbr = document.getElementsByClassName("mbr")[0];
+    var bigImg = document.getElementsByClassName("bigImg")[0].getElementsByTagName("img");
+    var IndexImg = 1;
+    var endImg = list.length;
+    var width = 126;
+    list[0].className="cur";
+    litImg.style.width = width*list.length+"px";
+
+    mbl.onclick = function(){
+        IndexImg = ImgLast(bigImg,IndexImg);
+        turnLeftImg();
+    }
+    mbr.onclick = function(){
+        IndexImg = ImgNext(bigImg,IndexImg);
+        turnRightImg();
+    }
+}
+function turnLeftImg(){
+    var litImg =document.getElementsByClassName("litImg")[0];
+    var list = litImg.getElementsByTagName("li");
+    var mbl = document.getElementsByClassName("mbl")[0];
+    var mbr = document.getElementsByClassName("mbr")[0];
+    var bigImg = document.getElementsByClassName("bigImg")[0].getElementsByTagName("img");
+    var IndexImg = 1;
+    var endImg = list.length;
+    var width = 126;
+    afterImg = turnLeft(litImg,list,afterImg,endImg,width);
+}
+function turnRightImg(){
+    var litImg =document.getElementsByClassName("litImg")[0];
+    var list = litImg.getElementsByTagName("li");
+    var mbl = document.getElementsByClassName("mbl")[0];
+    var mbr = document.getElementsByClassName("mbr")[0];
+    var bigImg = document.getElementsByClassName("bigImg")[0].getElementsByTagName("img");
+    var IndexImg = 1;
+    var endImg = list.length;
+    var width = 126;
+    afterImg = turnRight(litImg,list,afterImg,endImg,width);
+}
+//小图片向右横移
+function turnRight(litImg,list,a,endImg,width){
+    a++;
+    if(width*(a+5)>width*list.length){
+        if(a>=endImg){
+            a=0;
+            litImg.style.left = -0+"px";
+        }else{
+            litImg.style.left = -(endImg-5)*width+"px";
+        }
+    }else{
+        litImg.style.left = -a*width+"px";
+    }
+    for (var i = 0; i < list.length; i++) {
+        list[i].className = "";
+        if (i==a){
+            list[i].className="cur";
+        }
+    }
+    return a
+}
+//小图片向左横移
+function turnLeft(litImg,list,a,endImg,width){
+    a--;
+    if(a<5){
+        if(a<0){
+            a=endImg-1;
+            litImg.style.left = -(a-4)*width+"px";
+        }else{
+            litImg.style.left = -0+"px";
+        }
+    }else{
+        litImg.style.left = -(a-4)*width+"px";
+    }
+    for (var i = 0; i < list.length; i++) {
+        list[i].className = "";
+        if (i==(a-1)){
+            list[i].className="cur";
+        }
+    }
+    return a;
+}
+//大图片左右事件
+function BIGblAndbrFun(){
+    var bl = document.getElementsByClassName("bl")[0];
+    var br = document.getElementsByClassName("br")[0];
+    var bigImg = document.getElementsByClassName("bigImg")[0].getElementsByTagName("img");
+    var IndexImg = 1;
+    var endImg = bigImg.length;
+    bl.onmouseover = function(){
+        fadeIn(bl,500);
+    }
+    br.onmouseover = function(){
+        fadeIn(br,500)
+    }
+    bl.onmouseout = function(){
+        fadeOut(bl,200);
+    }
+    br.onmouseout = function(){
+        fadeOut(br,200)
+    }
+    bl.onclick = function(){
+        IndexImg = ImgLast(bigImg,IndexImg);
+        turnLeftImg();
+    }
+    br.onclick = function(){
+        IndexImg = ImgNext(bigImg,IndexImg);
+        turnRightImg();
+    }
+
+}
+//上一张
+function ImgLast(obj,IndexImg){
+    var endImg = obj.length;
+    if(IndexImg==1){
+        IndexImg=endImg;
+    }else{
+        IndexImg-=1;
+    }
+    for (var i = 0; i < obj.length; i++) {
+        if((i+1)==IndexImg){
+            if(IndexImg==endImg){
+                fadeOut(obj[0],800);
+            }else{
+                fadeOut(obj[i+1],800);
+            }
+            fadeIn(obj[i],800)
+            obj[i].style.zIndex  = 10;
+        }else{
+            obj[i].style.zIndex  = -1;
+        }
+    }
+    return IndexImg;
+}
+//下一张
+function ImgNext(obj,IndexImg){
+    var endImg = obj.length;
+    if(IndexImg==endImg){
+        IndexImg=1;
+    }else{
+        IndexImg+=1;
+    }
+    for (var i = 0; i < obj.length; i++) {
+        if((i+1)==IndexImg){
+            if(IndexImg==1){
+                fadeOut(obj[endImg-1],800);
+            }else{
+                fadeOut(obj[i-1],800);
+            }
+            fadeIn(obj[i],800)
+            obj[i].style.zIndex  = 10;
+        }else{
+            obj[i].style.zIndex  = -1;
+        }
+    }
+    return IndexImg;
+}
+
+
+//淡入
+function  fadeIn(obj,time) {
+    var startTime = new Date();
+    obj.style.opacity=0;
+    var timer = setInterval(function () {
+        var nowTime = new Date();
+        var prop = (nowTime-startTime)/time;
+        if(prop>=1){
+            prop = 1;
+            clearInterval(timer);
+        }
+        obj.style.opacity = prop;
+    },13);
+}
+//淡出
+function  fadeOut(obj,time) {
+    var startTime=new Date();
+    obj.style.opacity=1;//设置下初始值透明度
+    var timer=setInterval(function(){
+        var nowTime=new Date();
+        var prop=(nowTime-startTime)/time;
+        if(prop>=1){
+            prop=1;//设置最终系数值
+            clearInterval(timer);
+        }
+        obj.style.opacity=1-prop;//透明度公式： 初始值+系数*（结束值-初始值）
+    },13);//每隔13ms执行一次function函数
+}
---
+++ renting/zfxxgl_admin_qd/web/static/js/old/general.js
@@ -0,0 +1,23 @@
+// 隐藏显示信息，who必须为id，obj为class，index为索引
+function showHidden(who,obj,index){
+    var a  = document.getElementById(who);
+    var hid = document.getElementsByClassName(obj)[index];
+    a.onmouseover = function () {
+        hid.style.left = a.offsetLeft+"px";
+        hid.style.display="block"
+        a.style.background = "#ac0000";
+    }
+    a.onmouseout = function () {
+        a.style.background = "#c73733";
+        hid.style.display="none"
+    }
+}
+//返回顶部
+function toTop(){
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScroll > 0) {
+        //可以在下一帧开始时调用指定函数
+        window.requestAnimationFrame(toTop);
+        window.scrollTo (0,currentScroll - (currentScroll/5));
+    }
+}
