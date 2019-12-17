e70a350db815e50f8f22e3c2765c04b5c710c30e
1977870073 <1977870073@qq.com>
2019-12-17 10:04:44 +0800

    修改后台房源页面

--- renting/zfxxgl_admin_qd/web/static/js/old/selectHouse.js
+++ renting/zfxxgl_admin_qd/web/static/js/old/selectHouse.js
@@ -1,10 +1,39 @@
 window.onload = function(){
 	// query();
 }
-function query(){
+function query(num){
 	$.ajax({
-		
+		type: 'get',
+		url: '/web/do/house/init',
+		data: {page:num},
+		success: function(data){
+			renderHTML(data);
+		},
+		error{
+			
+		}
 	});
+}
+//将数据渲染到content
+function renderHTML(json){
+	if(json.queryInfo==null || json.queryInfo == undefined){
+		var content_html = "<div class='content-top'>出租房屋</div><div class='content-null'><span>您没有出租的房子！</span></div>";
+		$("#content").html(content_html);
+	}else{
+		$("#content-info").tmpl(json).appendTo("#content");
+		var page_startToend_html = "";
+		var page = json.page;
+		if(page!=null){
+			for(var i = page.start; i<=page.end; i++){
+				if(i==page.pagNum){
+					page_startToend_html+="<a href='#'  class='page_active'>"+i+"</a>";
+				}else{
+					page_startToend_html+="<a href='#' >"+i+"</a>";
+				}
+			}
+			$("#page_startToend").html(page_startToend_html);
+		}
+	}
 }
 //删除
 function send(id) {
